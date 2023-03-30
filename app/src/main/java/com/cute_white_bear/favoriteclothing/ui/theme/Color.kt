93ce8cfb65442b99afac7d14152215f1b7291b61
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-03-30 12:25:00 +0300

    Added new colors for the app theme

--- app/src/main/java/com/cute_white_bear/favoriteclothing/ui/theme/Color.kt
+++ app/src/main/java/com/cute_white_bear/favoriteclothing/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.cute_white_bear.favoriteclothing.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Black = Color(0xFF121212)
+val Red = Color(0xFFCC071E)
+val White = Color(0xFFE5E5E5)
+val Gray = Color(0xFF808080)
